6ad0a3cbfce5c2eb30590f3fa0c8749973130e34
Aksel121 <aksellutfu@gmail.com>
2024-08-07 14:51:02 -0400

    added

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "users" {
+  for_each = var.users
+  name = each.key
+  path = "/"
+}
+
+variable "users" {
+
+    type = list(string)
+  
+}
+
+output "user_name" {
+  value = [for user in aws_iam_user.users : user.user_name]
+}
